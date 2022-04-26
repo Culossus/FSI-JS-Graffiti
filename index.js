533ed278f0ab0d684c7de06ff7a7c9c3a40a1289
unknown <b.extra9@gmail.com>
2022-04-26 19:10:53 -0400

    DOM manipulation

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+let header = document.querySelector("#page-header");
 
+header.style.textAlign = "Right";
+
+let dogImages = document.querySelectorAll(".dog-image");
+
+for (let x = 0; x < dogImages.length; x++) {
+  dogImages[x].style.borderRadius = "50px";
+  dogImages[x].style.transform = "scaleY(-1)";
+}
 // Select some elements...
+let dogNames = document.querySelectorAll(".dog-name");
+
+for (let x = 0; x < dogNames.length; x++) {
+  dogNames[x].style.textAlign = "left";
+}
+
+let footer = document.querySelector(".footer");
+
+footer.style.color = "red";
+footer.style.borderStyle = "solid";
